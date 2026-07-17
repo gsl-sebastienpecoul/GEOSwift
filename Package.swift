788bfbba78ff7b149c8b9a2e1efbd23bafa75e57
gsl-sebastienpecoul <sebastien.pecoul-ext@aviv-group.com>
2026-07-17 18:18:46 +0200

    Reference geos xcframework at immutable release tag 11.2.0-b1

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "geos",
-            url: "https://github.com/gsl-sebastienpecoul/GEOSwift/releases/download/11.2.0-binary/geos.xcframework.zip",
+            url: "https://github.com/gsl-sebastienpecoul/GEOSwift/releases/download/11.2.0-b1/geos.xcframework.zip",
             checksum: "ff281779ea6e3a3d19c495d963da209605e37f39e59fde631235e65784c721da"
         ),
         .target(
